8253b6c86c35f1f5812819e91dbb58d14b4593c1
Person4494 <ltvcode@gmail.com>
2015-05-09 15:03:05 -0400

    a

--- src/main/java/com/person/withertech/withertech.java
+++ src/main/java/com/person/withertech/withertech.java
@@ -1,12 +1,26 @@
 package com.person.withertech;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "Wither-Tech", name = "Wither Tech", version = "1.7.10-1.0")
 public class withertech {
     @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event)
+    public void pre(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void post(FMLPostInitializationEvent event)
     {
 
     }
