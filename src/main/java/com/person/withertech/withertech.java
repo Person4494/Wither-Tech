bd10a259de56213ebdf76f55e280fdeff4c94b1e
Person4494 <ltvcode@gmail.com>
2015-05-09 14:54:35 -0400

    New changelist

---
+++ src/main/java/com/person/withertech/withertech.java
@@ -0,0 +1,13 @@
+package com.person.withertech;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "Wither-Tech", name = "Wither Tech", version = "1.7.10-1.0")
+public class withertech {
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+}
