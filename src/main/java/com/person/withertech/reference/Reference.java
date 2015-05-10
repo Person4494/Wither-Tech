173e31ee4ac24bddac987d33550d0e3eb60b08bf
Person4494 <ltvcode@gmail.com>
2015-05-10 10:14:34 -0400

    Added some stuff

---
+++ src/main/java/com/person/withertech/reference/Reference.java
@@ -0,0 +1,11 @@
+package com.person.withertech.reference;
+
+public class Reference
+{
+    public static final String MOD_ID = "Wither-Tech";
+    public static final String MOD_NAME = "Wither Tech";
+    public static final String FINGERPRINT = "N/A";
+    public static final String VERSION ="1.0";
+    public static final String SERVER_PROXY_CLASS = "com.person.withertech.proxy.ServerProxy";
+    public static final String CLIENT_PROXY_CLASS = "com.person.withertech.proxy.ClientProxy";
+}
--- src/main/java/com/person/withertech/withertech.java
+++ src/main/java/com/person/withertech/withertech.java
@@ -1,19 +1,20 @@
 package com.person.withertech;
 
 import com.person.withertech.proxy.IProxy;
+import com.person.withertech.reference.Reference;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "Wither-Tech", name = "Wither Tech", version = "1.7.10-1.0")
+@Mod(modid = Reference.MOD_ID , name = Reference.MOD_NAME, version = Reference.VERSION)
 public class withertech
 {
-    @Mod.Instance("Wither-Tech")
+    @Mod.Instance(Reference.MOD_ID)
     public static withertech instacne;
 
-    @SidedProxy(clientSide = "com.person.withertech.proxy.ClientProxy", serverSide = "com.person.withertech.proxy.ServerProxy")
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
     public static IProxy proxy;
 
     @Mod.EventHandler
